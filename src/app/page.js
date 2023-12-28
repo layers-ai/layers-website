34aaa93d65b0ac8679b327f48a62113b5e86cd9c
Ole Spaarmann <os@codeshift.co>
2023-12-28 19:13:03 +0100

    Add testimonial section to home page.

--- src/app/page.js
+++ src/app/page.js
@@ -2,18 +2,20 @@ import Features from "./components/features";
 import Hero from "./components/hero";
 import Header from "./components/header";
 import HowStart from "./components/how_start";
+import Testimonials from "./components/testimonials";
 
 export default function Home() {
   return (
     <div className="bg-white">
-      <div className="bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-gray-100 via-gold-200 to-savory-500">
+      <div className="bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-gray-100 via-gold-200 to-savory-600">
         {/* <div className="bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-savory-800 via-savory-400 to-phlox-500"> */}
         <Header />
         <main>
-          <Hero />
+          <Hero id="hero" />
           <div className="bg-gradient-to-b from-white/0 via-white/50 to-white/100">
             <HowStart />
           </div>
+          <Testimonials />
         </main>
       </div>
     </div>
