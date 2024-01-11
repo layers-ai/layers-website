be8358902c0f304f40fb7c2096ad3f020310e23b
Ole Spaarmann <os@codeshift.co>
2024-01-11 17:28:58 +0100

    Fix typo.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -70,7 +70,7 @@ export default function Header() {
         <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Layers</span>
+              <span className="sr-only">layers</span>
               <img
                 className="h-8 w-auto"
                 src="/images/layers-logo-icon-only.svg"
