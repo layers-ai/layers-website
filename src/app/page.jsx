5d18849236f09b2e15024af3f3efa00bf2f8bbc4
Ole Spaarmann <os@codeshift.co>
2024-01-16 10:40:07 +0100

    First working version of waitlist with count.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -27,18 +27,25 @@ const cookiesClient = generateServerClientUsingCookies({
 
 async function countWaitlist() {
   "use server";
-  // Fetch count
-  const { data, errors } = await cookiesClient.graphql({
-    query: queries.listWaitlists,
-  });
+  const currentStage = process.env.AWS_AMPLIFY_ENV || "dev";
 
-  const users = data.listWaitlists.items;
-
-  return users && users.length > 0 && !errors ? users.length : 0;
+  try {
+    const response = await fetch(
+      `https://d5qp6v9we5.execute-api.us-east-1.amazonaws.com/${currentStage}/waitlistcount`
+    );
+    const data = await response.json();
+    console.log(data);
+    return data.count;
+  } catch (error) {
+    console.error(error);
+    return 0;
+  }
 }
 
 async function createWaitlist(formData) {
   "use server";
+  console.log("Creating Waitlist: ", formData.get("email"));
+
   const { data } = await cookiesClient.graphql({
     query: mutations.createWaitlist,
     variables: {
@@ -51,7 +58,7 @@ async function createWaitlist(formData) {
   console.log("Created Waitlist: ", data?.createWaitlist);
 
   let userCount = await countWaitlist();
-  console.log(`You are number ${5400 + userCount} on the waitlist!`);
+  console.log(`You are number ${userCount} on the waitlist!`);
 
   revalidatePath("/");
 }
