a69b26202d398b12dcd6426ea074d9b0464895ca
Ole Spaarmann <os@codeshift.co>
2024-07-26 21:25:37 +0200

    Add 404 page.

--- src/app/layout.js
+++ src/app/layout.js
@@ -75,8 +75,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${circular.variable} font-sans`}>
+    <html lang="en" className="h-full">
+      <body className={`${circular.variable} h-full font-sans`}>
         {children}
         <SpeedInsights />
       </body>
